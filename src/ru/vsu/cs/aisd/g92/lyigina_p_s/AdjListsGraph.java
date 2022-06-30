4d6ff4d22cb6435b8eb3885771c0123634c1ae63
Haku <polina231003@mail.ru>
2022-06-30 10:47:14 +0300

    Weighted graph

--- src/ru/vsu/cs/aisd/g92/lyigina_p_s/AdjListsGraph.java
+++ src/ru/vsu/cs/aisd/g92/lyigina_p_s/AdjListsGraph.java
@@ -8,8 +8,8 @@ import java.util.List;
 /**
  * Реализация графа на основе списков смежности
  */
-public class AdjListsGraph implements Graph {
-    private List<List<Integer>> vAdjLists = new ArrayList<>();
+public class AdjListsGraph implements WeightedGraph {
+    private List<List<WeightedEdgeTo>> vAdjLists = new ArrayList<>();
     private int vCount = 0;
     private int eCount = 0;
 
@@ -30,6 +30,23 @@ public class AdjListsGraph implements Graph {
         }
     };
 
+    private static Iterable<WeightedEdgeTo> nullIterableWeighted = new Iterable<WeightedEdgeTo>() {
+        @Override
+        public Iterator<WeightedEdgeTo> iterator() {
+            return new Iterator<>() {
+                @Override
+                public boolean hasNext() {
+                    return false;
+                }
+
+                @Override
+                public WeightedEdgeTo next() {
+                    return null;
+                }
+            };
+        }
+    };
+
     @Override
     public int vertexCount() {
         return vCount;
@@ -42,29 +59,14 @@ public class AdjListsGraph implements Graph {
 
     @Override
     public void addEdge(int v1, int v2) {
-        int maxV = Math.max(v1, v2);
-        // добавляем вершин в список списков связности
-        for (; vCount <= maxV; vCount++) {
-            vAdjLists.add(null);
-        }
-        if (!isAdj(v1, v2)) {
-            if (vAdjLists.get(v1) == null) {
-                vAdjLists.set(v1, new LinkedList<>());
-            }
-            vAdjLists.get(v1).add(v2);
-            eCount++;
-            if (vAdjLists.get(v2) == null) {
-                vAdjLists.set(v2, new LinkedList<>());
-            }
-            vAdjLists.get(v2).add(v1);
-        }
+
     }
 
-    private int countingRemove(List<Integer> list, int v) {
+    private int countingRemove(List<WeightedEdgeTo> list, int v) {
         int count = 0;
         if (list != null) {
-            for (Iterator<Integer> it = list.iterator(); it.hasNext(); ) {
-                if (it.next().equals(v)) {
+            for (Iterator<WeightedEdgeTo> it = list.iterator(); it.hasNext(); ) {
+                if (it.next().to() == v) {
                     it.remove();
                     count++;
                 }
@@ -81,9 +83,55 @@ public class AdjListsGraph implements Graph {
 
     @Override
     public Iterable<Integer> adjacencies(int v) {
-        return vAdjLists.get(v) == null ? nullIterable : vAdjLists.get(v);
+        return nullIterable;
+        //return vAdjLists.get(v) == null ? nullIterable : vAdjLists.get(v);
     }
 
     @Override
     public int getAdjCount(int v){return vAdjLists.get(v).size();}
+
+    @Override
+    public void addEdge(int v1, int v2, double weight) {
+        int maxV = Math.max(v1, v2);
+        // добавляем вершин в список списков связности
+        for (; vCount <= maxV; vCount++) {
+            vAdjLists.add(null);
+        }
+        if (!isAdj(v1, v2)) {
+            if (vAdjLists.get(v1) == null) {
+                vAdjLists.set(v1, new LinkedList<>());
+            }
+            vAdjLists.get(v1).add(new WeightedEdgeTo() {
+                @Override
+                public int to() {
+                    return v2;
+                }
+
+                @Override
+                public double weight() {
+                    return weight;
+                }
+            });
+            eCount++;
+            if (vAdjLists.get(v2) == null) {
+                vAdjLists.set(v2, new LinkedList<>());
+            }
+            vAdjLists.get(v2).add(new WeightedEdgeTo() {
+                @Override
+                public int to() {
+                    return v1;
+                }
+
+                @Override
+                public double weight() {
+                    return weight;
+                }
+            });
+        }
+    }
+
+    @Override
+    public Iterable<WeightedEdgeTo> adjacenciesWithWeights(int v) {
+        return vAdjLists.get(v) == null ? nullIterableWeighted : vAdjLists.get(v);
+    }
 }
